2e038b834f01cd1c652fdb987b1edd4fcefc2c3a
bhavasagar09 <bhavasagar.putta@quantrium.ai>
2023-04-02 19:21:06 +0530

    "Final Interference Analaysis with Volume Calculation"

--- interfrence.py
+++ interfrence.py
@@ -1,5 +1,8 @@
-import bpy
-import bmesh
+import bpy, bmesh
+from mathutils.bvhtree import BVHTree
+
+# Treshold volume is used to prevent bad CAD files from being analyzed as overlaps.
+TRESHOLD_VOLUME = 10**(-2)
 
 def print(*data):
     for window in bpy.context.window_manager.windows:
@@ -13,22 +16,53 @@ def print(*data):
 obj1 = bpy.data.objects['base_link']
 obj2 = bpy.data.objects['obj2']
 
-# Create a BMesh object for each mesh
+def CalculateVolumeofOverlap(obj1, obj2):
+    # TODO: Copy the object 1 before applying boolean operation
+    # Intersect the two BMesh objects
+    mod = obj1.modifiers.new("Boolean", type='BOOLEAN')
+    mod.operation = 'INTERSECT'
+    mod.object = obj2
+    bpy.ops.object.modifier_apply({"object": obj1},modifier=mod.name)
 
-# bm2 = bmesh.new()
-# bm2.from_mesh(mesh2)
-bm = bmesh.new()
-bm.from_mesh(bpy.data.objects['base_link'].to_mesh())
-print("Volume of", obj1.name, "is", bm.calc_volume())
+    bm1 = bmesh.new()
+    bm1.from_mesh(obj1.data)
 
-# Intersect the two BMesh objects
-mod = obj1.modifiers.new("Boolean", type='BOOLEAN')
-mod.operation = 'INTERSECT'
-mod.object = obj2
-bpy.ops.object.modifier_apply({"object": obj1},modifier=mod.name)
+    intersection_volume = bm1.calc_volume(signed=True)
 
-bm1 = bmesh.new()
-bm1.from_mesh(bpy.data.objects['base_link'].data)
+    # Print the volume of the object
+    print("Volume of", obj1.name, "is", intersection_volume)
+    return intersection_volume
 
-# Print the volume of the object
-print("Volume of", obj1.name, "is", bm1.calc_volume(signed=True))
+def OverlapCheck(obj1, obj2):
+    # Get their world matrix
+    mat1 = obj1.matrix_world
+    mat2 = obj2.matrix_world
+
+    # Get the geometry in world coordinates
+    vert1 = [mat1 @ v.co for v in obj1.data.vertices] 
+    poly1 = [p.vertices for p in obj1.data.polygons]
+
+    vert2 = [mat2 @ v.co for v in obj2.data.vertices] 
+    poly2 = [p.vertices for p in obj2.data.polygons]
+
+    # Create the BVH trees
+    bvh1 = BVHTree.FromPolygons( vert1, poly1 )
+    bvh2 = BVHTree.FromPolygons( vert2, poly2 )
+
+    inter = bvh1.overlap(bvh2)
+    print(bvh1)
+
+    # Test if overlap
+    if inter != []:
+        intrersection_volume = CalculateVolumeofOverlap(obj1, obj2)
+        if intrersection_volume > TRESHOLD_VOLUME:
+            print("Overlap")
+        else:
+            print("Touching")
+
+    else:
+        print("No overlap or touching")
+
+
+if __name__ == "__main__":
+    OverlapCheck(obj1, obj2)
--- test/test.py
+++ test/test.py
@@ -53,6 +53,75 @@ mesh2 = bpy.data.objects['obj2'].to_mesh()
 # bpy.data.meshes.remove(mesh2)
 
 
+# import bpy, bmesh
+# from mathutils.bvhtree import BVHTree
+
+# # Treshold volume is used to prevent bad CAD files from being analyzed as overlaps.
+# TRESHOLD_VOLUME = 10**(-2)
+
+# def print(*data):
+#     for window in bpy.context.window_manager.windows:
+#         screen = window.screen
+#         for area in screen.areas:
+#             if area.type == 'CONSOLE':
+#                 override = {'window': window, 'screen': screen, 'area': area}
+#                 bpy.ops.console.scrollback_append(override, text=str(data), type="OUTPUT")
+
+# # Get the objects
+# obj1 = bpy.data.objects['base_link']
+# obj2 = bpy.data.objects['obj2']
+
+# # bm = bmesh.new()
+# # bm.from_mesh(bpy.data.objects['base_link'].to_mesh())
+# # print("Volume of", obj1.name, "is", bm.calc_volume())
+
+# def CalculateVolumeofOverlap(obj1, obj2):
+#     # TODO: Copy the object 1 before applying boolean operation
+#     # Intersect the two BMesh objects
+#     mod = obj1.modifiers.new("Boolean", type='BOOLEAN')
+#     mod.operation = 'INTERSECT'
+#     mod.object = obj2
+#     bpy.ops.object.modifier_apply({"object": obj1},modifier=mod.name)
+
+#     bm1 = bmesh.new()
+#     bm1.from_mesh(obj1.data)
+
+#     intersection_volume = bm1.calc_volume(signed=True)
+
+#     # Print the volume of the object
+#     print("Volume of", obj1.name, "is", intersection_volume)
+#     return intersection_volume
+
+# def OverlapCheck(obj1, obj2):
+#     # Get their world matrix
+#     mat1 = obj1.matrix_world
+#     mat2 = obj2.matrix_world
+
+#     # Get the geometry in world coordinates
+#     vert1 = [mat1 @ v.co for v in obj1.data.vertices] 
+#     poly1 = [p.vertices for p in obj1.data.polygons]
+
+#     vert2 = [mat2 @ v.co for v in obj2.data.vertices] 
+#     poly2 = [p.vertices for p in obj2.data.polygons]
+
+#     # Create the BVH trees
+#     bvh1 = BVHTree.FromPolygons( vert1, poly1 )
+#     bvh2 = BVHTree.FromPolygons( vert2, poly2 )
+
+#     inter = bvh1.overlap(bvh2)
+#     print(bvh1)
+
+#     # Test if overlap
+#     if inter != []:
+#         intrersection_volume = CalculateVolumeofOverlap(obj1, obj2)
+#         if intrersection_volume > TRESHOLD_VOLUME:
+#             print("Overlap")
+#         else:
+#             print("Touching")
+
+#     else:
+#         print("No overlap or touching")
+
 # Apply a boolean modifier to intersect the two objects
 mod = obj1.modifiers.new("Boolean", type='BOOLEAN')
 mod.operation = 'INTERSECT'
